5a7f5cd24167aae3aed69d1e8d159a50b7de4c54
Derrick Marfo <steammarfo111@gmail.com>
2025-02-12 02:49:28 +0000

    Firstscreen(row1_completed)

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
           <p className="social_text">Socials</p>
           <div className="social_list_box">
             <ul className="social_list">
-              <li>Gmail: <a href="www.google.com" target="blank">marfoofficial111@gmail.com</a></li> 
+              <li>Gmail(<a className="gmail" target="blank" href="https://mail.google.com/mail/?view=cm&fs=1&to=marfoofficial111@gmail.com">marfoofficial111</a>)</li> 
               <li>LinkedIn</li> 
               <li>GitHub</li> 
               <li>Resume</li> 
